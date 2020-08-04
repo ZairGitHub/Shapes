5e8016fd975869429ed4ec96e93defa2b4637914
Zair <michaelxzr@gmail.com>
2020-08-04 20:22:03 +0100

    Basic Layout
    Movable player which resets when leaving the floor boundary.

---
+++ Shapes/Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 offset;
+    
+    private void Start()
+    {
+        offset = new Vector3(0, 20, 0);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+        transform.position = offset;
+    }
+}
---
+++ Shapes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    private void Start()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.MovePosition(Vector3.up);
+        rb.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        movement = new Vector3(horizontalAxis, 0.0f, verticalAxis);
+        rb.velocity = movement * speed + new Vector3(0.0f, rb.velocity.y, 0.0f);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            Reset();
+        }
+    }
+}
