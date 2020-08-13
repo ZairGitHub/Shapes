b220da7d582075d1989759517895c5a45ae53d22
Zair <michaelxzr@gmail.com>
2020-08-13 15:53:19 +0100

    Added a TimeController class for handling time.
    GameController now accesses TimeController to modify time.

--- Shapes/Assets/Scripts/GameController.cs
+++ Shapes/Assets/Scripts/GameController.cs
@@ -2,16 +2,38 @@
 
 public class GameController : MonoBehaviour
 {
+    private TimeController timeController;
+
+    private void Start()
+    {
+        timeController = GameObject.FindGameObjectWithTag("TimeController").GetComponent<TimeController>();
+    }
+
     private void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            TogglePauseMenu();
+            timeController.TogglePause();
         }
-    }
+        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            timeController.PauseTime();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            timeController.ResumeTime();
+        }
 
-    private void TogglePauseMenu()
-    {
-        Time.timeScale = (Time.timeScale == 1) ? 0 : 1;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            timeController.SlowDownTime();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            timeController.SpeedUpTime();
+        }
     }
 }
---
+++ Shapes/Assets/Scripts/TimeController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    private float timeFactor;
+
+    private void Start()
+    {
+        timeFactor = 0.1f;
+    }
+
+    public void TogglePause()
+    {
+        Time.timeScale = (Time.timeScale == 1) ? 0 : 1;
+    }
+
+    public void PauseTime()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeTime()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void SlowDownTime()
+    {
+        Time.timeScale = (Time.timeScale - timeFactor > 0) ? Time.timeScale -= timeFactor : 0;
+    }
+
+    public void SpeedUpTime()
+    {
+        Time.timeScale = (Time.timeScale + timeFactor < 1) ? Time.timeScale += timeFactor : 1;
+    }
+
+    public void SetTime(float timeModifier)
+    {
+        Time.timeScale = timeModifier;
+    }
+}
