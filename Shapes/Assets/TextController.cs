4ca34102c308a73813d90951e5bd33cf9e5261c5
Zair <michaelxzr@gmail.com>
2020-08-13 16:05:05 +0100

    Added a TextController script for handling text.
    TextController now displays text for monitoring time.

---
+++ Shapes/Assets/TextController.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    TMP_Text textDebugTime;
+
+    private void Start()
+    {
+        textDebugTime = GameObject.FindGameObjectWithTag("TextDebugTime").GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        textDebugTime.text = "Time: " + Time.timeScale.ToString("0.00");
+    }
+}
