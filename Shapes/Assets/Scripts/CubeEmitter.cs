1247c2b3a2b31015cf24bff53d0fb82076e5eca2
Zair <michaelxzr@gmail.com>
2020-08-16 00:39:01 +0100

    Slight clean-up to the CubeEmitter class.
    CubeEmitter now fullly supports a variable number of CubeEmitter GameObjects.

--- Shapes/Assets/Scripts/CubeEmitter.cs
+++ Shapes/Assets/Scripts/CubeEmitter.cs
@@ -20,13 +20,13 @@ public class CubeEmitter : MonoBehaviour
     {
         while (true)
         {
-            int RNG = Random.Range(0, 8);
+            int RNG = Random.Range(0, cubeEmitters.Length);
             cubeEmitters[RNG].GetComponent<Renderer>().material.color = Color.blue;
+            emitterDirection = cubeEmitters[RNG].GetComponent<EmitterDirection>();
+
             yield return new WaitForSeconds(1);
 
-
             CubeHandler cubeObject = Instantiate(cube, cubeEmitters[RNG].transform.position + Vector3.up, Quaternion.identity).GetComponent<CubeHandler>();
-            emitterDirection = cubeEmitters[RNG].GetComponent<EmitterDirection>();
             cubeObject.SetDirection(emitterDirection.GetXDirection(), emitterDirection.GetZDirection());
 
             cubeEmitters[RNG].GetComponent<Renderer>().material.color = Color.yellow;
