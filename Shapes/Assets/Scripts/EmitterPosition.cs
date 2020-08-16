8a0d08e1ad137c4b3081020f6fc9ab5926acc79d
Zair <michaelxzr@gmail.com>
2020-08-16 15:31:47 +0100

    Cleaned up EmitterPosition.
    EmitterPosition's switch statement has been moved from its Start() method to its SetPosition() method.
    SetPosition() has been refactored to prevent repeated declarations of assignment.

--- Shapes/Assets/Scripts/EmitterPosition.cs
+++ Shapes/Assets/Scripts/EmitterPosition.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Assertions.Must;
 
 public class EmitterPosition : MonoBehaviour
 {
@@ -13,46 +12,43 @@ public class EmitterPosition : MonoBehaviour
     {
         constants = GameObject.FindGameObjectWithTag("Constants").GetComponent<Constants>();
         boundaryOffset = GetComponent<Renderer>().bounds.size.x * 2;
-
-        switch (name)
-        {
-            case string a when a.Contains("TopLeft"):
-                xPosition = -constants.GetBoundaryWidth() + boundaryOffset;
-                zPosition = constants.GetBoundaryHeight() - boundaryOffset;
-                break;
-            case string a when a.Contains("TopMiddle"):
-                xPosition = 0.0f;
-                zPosition = constants.GetBoundaryHeight() - boundaryOffset;
-                break;
-            case string a when a.Contains("TopRight"):
-                xPosition = constants.GetBoundaryWidth() - boundaryOffset;
-                zPosition = constants.GetBoundaryHeight() - boundaryOffset;
-                break;
-            case string a when a.Contains("MiddleLeft"):
-                xPosition = -constants.GetBoundaryWidth() + boundaryOffset;
-                zPosition = 0.0f;
-                break;
-            case string a when a.Contains("MiddleRight"):
-                xPosition = constants.GetBoundaryWidth() - boundaryOffset;
-                zPosition = 0.0f;
-                break;
-            case string a when a.Contains("BottomLeft"):
-                xPosition = -constants.GetBoundaryWidth() + boundaryOffset;
-                zPosition = -constants.GetBoundaryHeight() + boundaryOffset;
-                break;
-            case string a when a.Contains("BottomMiddle"):
-                xPosition = 0.0f;
-                zPosition = -constants.GetBoundaryHeight() + boundaryOffset;
-                break;
-            case string a when a.Contains("BottomRight"):
-                xPosition = constants.GetBoundaryWidth() - boundaryOffset;
-                zPosition = -constants.GetBoundaryHeight() + boundaryOffset;
-                break;
-        }
     }
 
     public Vector3 SetPosition()
     {
+        switch (name)
+        {
+            case string a when a.Contains("Top"):
+                zPosition = constants.GetBoundaryHeight() - boundaryOffset;
+                if (a.Contains("Left"))
+                {
+                    xPosition = -constants.GetBoundaryWidth() + boundaryOffset;
+                }
+                else if (a.Contains("Right"))
+                {
+                    xPosition = constants.GetBoundaryWidth() - boundaryOffset;
+                }
+                break;
+
+            case string a when a.Contains("MiddleLeft"):
+                xPosition = -constants.GetBoundaryWidth() + boundaryOffset;
+                break;
+            case string a when a.Contains("MiddleRight"):
+                xPosition = constants.GetBoundaryWidth() - boundaryOffset;
+                break;
+
+            case string a when a.Contains("Bottom"):
+                zPosition = -constants.GetBoundaryHeight() + boundaryOffset;
+                if (a.Contains("Left"))
+                {
+                    xPosition = -constants.GetBoundaryWidth() + boundaryOffset;
+                }
+                else if (a.Contains("Right"))
+                {
+                    xPosition = constants.GetBoundaryWidth() - boundaryOffset;
+                }
+                break;
+        }
         return new Vector3(xPosition, 0.0f, zPosition);
     }
 }
