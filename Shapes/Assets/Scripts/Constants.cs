637029594edf3bd283f6ff089d819ef9f3b87e6d
Zair <michaelxzr@gmail.com>
2020-12-20 13:17:35 +0000

    Reorder property declaration to alphabetical order in Constants.cs

--- Shapes/Assets/Scripts/Constants.cs
+++ Shapes/Assets/Scripts/Constants.cs
@@ -2,15 +2,15 @@
 
 public class Constants : MonoBehaviour
 {
-    public float BoundaryWidth { get; private set; }
     public float BoundaryHeight { get; private set; }
+    public float BoundaryWidth { get; private set; }
 
     private void Awake()
     {
-        BoundaryWidth = (GameObject.FindGameObjectWithTag("BoundaryWest").transform.position.x
-            - GameObject.FindGameObjectWithTag("BoundaryEast").transform.position.x) / 2.0f;
-
         BoundaryHeight = (GameObject.FindGameObjectWithTag("BoundaryNorth").transform.position.y
             - GameObject.FindGameObjectWithTag("BoundarySouth").transform.position.y) / 2.0f;
+
+        BoundaryWidth = (GameObject.FindGameObjectWithTag("BoundaryWest").transform.position.x
+            - GameObject.FindGameObjectWithTag("BoundaryEast").transform.position.x) / 2.0f;
     }
 }
