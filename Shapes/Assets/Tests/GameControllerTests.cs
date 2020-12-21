ffe0dcb735005e1c84c7ed670fcecddb8932e3f4
Zair <michaelxzr@gmail.com>
2020-12-21 11:25:46 +0000

    Add tests for new GameControllerTests.cs

---
+++ Shapes/Assets/Tests/GameControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GameControllerTests
+    {
+        [UnityTest]
+        public IEnumerable IsRunnningProperty_HasDefaultValueOfTrue()
+        {
+            var sut = new GameObject().AddComponent<GameController>();
+            yield return null;
+
+            var result = sut.IsRunning;
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Reset_SetsIsRunnningToFalse()
+        {
+            var sut = new GameObject().AddComponent<GameController>();
+            sut.Reset();
+
+            var result = sut.IsRunning;
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
