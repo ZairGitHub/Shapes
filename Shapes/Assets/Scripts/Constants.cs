bc5bea9f49d8060af59ad663886e9df217ab4976
Zair <michaelxzr@gmail.com>
2021-01-11 11:59:06 +0000

    Update Constants.cs

--- Shapes/Assets/Scripts/Constants.cs
+++ Shapes/Assets/Scripts/Constants.cs
@@ -7,6 +7,14 @@ public class Constants : MonoBehaviour
     public float GameWidth { get; private set; }
     public float GameHeight { get; private set; }
 
+    private enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
     private void Awake()
     {
         BoundaryWidth = GameObject.FindGameObjectWithTag("BoundaryEast")
