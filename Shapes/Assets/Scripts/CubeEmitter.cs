d533124195c59040c57f5b12112353c4d772c20b
Zair <michaelxzr@gmail.com>
2021-01-12 22:03:47 +0000

    Revert "Move external initialisation from Awake() to Start() method in CubeEmitter.cs"
    This reverts commit c717b61aa2a5f5df7f39b5a2503b04cefb784efe.

--- Shapes/Assets/Scripts/CubeEmitter.cs
+++ Shapes/Assets/Scripts/CubeEmitter.cs
@@ -11,7 +11,7 @@ public class CubeEmitter : MonoBehaviour
     private GameController _gameController;
     private EmitterProperties _emitterProperties;
 
-    private void Start()
+    private void Awake()
     {
         _cube = GameObject.FindGameObjectWithTag("Cube");
         _cubeEmitters = GameObject.FindGameObjectsWithTag("CubeEmitter")
@@ -19,7 +19,10 @@ public class CubeEmitter : MonoBehaviour
 
         _gameController = GameObject.FindGameObjectWithTag("GameController")
             .GetComponent<GameController>();
+    }
 
+    private void Start()
+    {
         foreach (GameObject cubeEmitter in _cubeEmitters)
         {
             cubeEmitter.transform.position =
