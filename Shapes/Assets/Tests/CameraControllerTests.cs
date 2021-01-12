6eee4fd1dc215417c35d5d2727dd35f2717617c8
Zair <michaelxzr@gmail.com>
2021-01-12 21:24:46 +0000

    Create CameraControllerTests.cs

---
+++ Shapes/Assets/Tests/CameraControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CameraControllerTests
+    {
+        [Test]
+        public void CameraControllerTestsSimplePasses()
+        {
+            var sut = new GameObject().AddComponent<CameraController>();
+            sut.runInEditMode = true;
+
+            var result = sut.transform.position;
+
+            Assert.That(result, Is.EqualTo(new Vector3(0.0f, 0.0f, -10.0f)));
+        }
+    }
+}
