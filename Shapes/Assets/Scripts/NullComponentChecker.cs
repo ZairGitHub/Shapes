083b9b0cbf94f2a308c04052289098cb8765a307
Zair <michaelxzr@gmail.com>
2021-01-18 23:00:50 +0000

    Add methods to NullComponentChecker.cs

--- Shapes/Assets/Scripts/NullComponentChecker.cs
+++ Shapes/Assets/Scripts/NullComponentChecker.cs
@@ -11,4 +11,14 @@ public static class NullComponentChecker
         }
         return component == null ? gameObject.AddComponent<T>() : component;
     }
+
+    public static GameObject TryGet(GameObject gameObject)
+    {
+        return gameObject == null ? new GameObject() : gameObject;
+    }
+
+    public static GameObject[] TryGet(GameObject[] gameObjects)
+    {
+        return gameObjects ?? new GameObject[1];
+    }
 }
