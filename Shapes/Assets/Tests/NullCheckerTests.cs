646660cb03662e1c893f32890ca32258e3631f2b
Zair <michaelxzr@gmail.com>
2021-01-19 19:06:42 +0000

    Fix oversight that casted generic parameter regardless of component argument type

--- Shapes/Assets/Tests/NullCheckerTests.cs
+++ Shapes/Assets/Tests/NullCheckerTests.cs
@@ -3,43 +3,62 @@ using UnityEngine;
 
 namespace Tests
 {
+    [TestFixture]
     public class NullCheckerTests
     {
         [Test]
-        public void TryGet_NullArguments_ReturnsNull()
+        public void TryGet_NullComponent_ReturnsComponentOfGenericType()
         {
-            var result = NullChecker.TryGet<Rigidbody>(null, null);
+            var result = NullChecker.TryGet<Rigidbody>(null);
 
-            Assert.That(result, Is.Null);
-        }
-
-        [Test]
-        public void TryGet_NullGameObject_ReturnsNull()
-        {
-            var result = NullChecker
-                .TryGet<Rigidbody>(null, new GameObject().GetComponent<Rigidbody>());
-
-            Assert.That(result, Is.Null);
-        }
-
-        [Test]
-        public void TryGet_NullComponent_AddsComponentTypeToGameObject()
-        {
-            var result = NullChecker
-                .TryGet<Rigidbody>(new GameObject(), null);
-
-            Assert.That(result, Is.InstanceOf<Component>().And.TypeOf<Rigidbody>());
+            Assert.That(result, Is.TypeOf<Rigidbody>());
         }
 
         [Test]
         public void TryGet_Component_ReturnsComponent()
+        {
+            var component = new GameObject().AddComponent<Rigidbody>();
+
+            var result = NullChecker.TryGet(component);
+
+            Assert.That(result, Is.TypeOf<Rigidbody>());
+        }
+
+        [Test]
+        public void TryGet_NullArguments_ReturnsComponentOfGenericType()
+        {
+            var result = NullChecker.TryGet<Rigidbody>(null, null);
+
+            Assert.That(result, Is.TypeOf<Rigidbody>());
+        }
+
+        [Test]
+        public void TryGet_NullGameObject_ReturnsComponentOfGenericType()
+        {
+            var component = new GameObject().GetComponent<Rigidbody>();
+
+            var result = NullChecker.TryGet<Rigidbody>(null, component);
+
+            Assert.That(result, Is.TypeOf<Rigidbody>());
+        }
+
+        [Test]
+        public void TryGet_NullComponentGameObejct_ReturnsComponentOfGenericType()
+        {
+            var result = NullChecker.TryGet<Rigidbody>(new GameObject(), null);
+
+            Assert.That(result, Is.TypeOf<Rigidbody>());
+        }
+
+        [Test]
+        public void TryGet_GameObjectAndComponent_ReturnsComponent()
         {
             var gameObject = new GameObject();
 
             var result = NullChecker
                 .TryGet<Rigidbody>(gameObject, gameObject.GetComponent<Rigidbody>());
 
-            Assert.That(result, Is.InstanceOf<Component>().And.TypeOf<Rigidbody>());
+            Assert.That(result, Is.TypeOf<Rigidbody>());
         }
     }
 }
