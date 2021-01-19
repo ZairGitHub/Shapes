af7f699d72f588ea36013f74c0ebb6b6a375f1db
Zair <michaelxzr@gmail.com>
2021-01-19 19:59:49 +0000

    Update SphereEmitter.cs

--- Shapes/Assets/Scripts/SphereEmitter.cs
+++ Shapes/Assets/Scripts/SphereEmitter.cs
@@ -5,14 +5,20 @@ public class SphereEmitter : MonoBehaviour
 {
     private readonly WaitForSeconds _emitterDelay = new WaitForSeconds(3.0f);
 
+    private IGameController _gameController;
     private GameObject _sphere;
-    private GameController _gameController;
 
     private void Start()
     {
+        _gameController = (GameController)NullChecker.TryGet<GameController>(gameObject,
+                    GameObject.FindWithTag("GameController").GetComponent<GameController>());
+
         _sphere = GameObject.FindWithTag("Sphere");
-        _gameController = GameObject.FindWithTag("GameController")
-            .GetComponent<GameController>();
+        if (_sphere == null)
+        {
+            _sphere = new GameObject();
+            _sphere.AddComponent<SphereHandler>();
+        }
 
         StartCoroutine(EmitSpheres());
     }
