665174be5e1a7ca5c5ca44ac3a45039ec49f17ee
Zair <michaelxzr@gmail.com>
2021-01-20 20:58:08 +0000

    Create Movement.cs

---
+++ Shapes/Assets/Scripts/Movement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Movement : IMovable
+{
+    public Vector3 Move(float horizontal, float vertical)
+    {
+        return new Vector3(horizontal, vertical, 0.0f);
+    }
+}
