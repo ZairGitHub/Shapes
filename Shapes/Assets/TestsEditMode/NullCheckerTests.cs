8731a8a8a84586cb16d7f66bd93a0b9584668b5c
Zair <michaelxzr@gmail.com>
2021-01-20 18:41:35 +0000

    Remove tests from NullCheckerTests.cs

--- Shapes/Assets/TestsEditMode/NullCheckerTests.cs
+++ Shapes/Assets/TestsEditMode/NullCheckerTests.cs
@@ -10,35 +10,11 @@ namespace Tests
         private readonly Component _component = new GameObject().AddComponent<Rigidbody>();
 
         [Test]
-        public void TryGet_NullComponent_ReturnsComponent()
-        {
-            var result = NullChecker.TryGet<Rigidbody>(null);
-
-            Assert.That(result, Is.TypeOf<Rigidbody>());
-        }
-
-        [Test]
-        public void TryGet_Component_ReturnsComponent()
-        {
-            var result = NullChecker.TryGet<Rigidbody>(_component);
-
-            Assert.That(result, Is.TypeOf<Rigidbody>());
-        }
-
-        [Test]
-        public void TryGet_NullArguments_ReturnsComponent()
-        {
-            var result = NullChecker.TryGet<Rigidbody>(null, null);
-
-            Assert.That(result, Is.TypeOf<Rigidbody>());
-        }
-
-        [Test]
-        public void TryGet_NullGameObjectAndComponent_ReturnsComponent()
+        public void TryGet_NullGameObject_ReturnsNull()
         {
             var result = NullChecker.TryGet<Rigidbody>(null, _component);
 
-            Assert.That(result, Is.TypeOf<Rigidbody>());
+            Assert.That(result, Is.Null);
         }
 
         [Test]
