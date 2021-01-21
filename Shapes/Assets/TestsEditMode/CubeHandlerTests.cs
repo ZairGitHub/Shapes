1c474bf9ea677b7bbbdf5258e4f0fc8a9d464a35
Zair <michaelxzr@gmail.com>
2021-01-21 17:39:59 +0000

    Add [Test] to CubeHandlerTests.cs

--- Shapes/Assets/TestsEditMode/CubeHandlerTests.cs
+++ Shapes/Assets/TestsEditMode/CubeHandlerTests.cs
@@ -12,12 +12,22 @@ namespace Tests
         public void HasSpeed_SpeedIsNotPositive_ReturnsFalse()
         {
             var sut = new GameObject().AddComponent<CubeHandler>();
-            sut.runInEditMode = true;
             sut.RunTestingConstructor(0.0f);
 
             var result = sut.HasSpeed();
 
             Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void HasSpeed_SpeedIsPositive_ReturnsTrue()
+        {
+            var sut = new GameObject().AddComponent<CubeHandler>();
+            sut.RunTestingConstructor(float.Epsilon);
+
+            var result = sut.HasSpeed();
+
+            Assert.That(result, Is.True);
         }
     }
 }
