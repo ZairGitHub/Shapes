ade79b3cdfa6577d00d9e7c8a968c8d0ee360246
Zair <michaelxzr@gmail.com>
2021-01-21 17:43:37 +0000

    Add helper method to CubeHandlerTests.cs

--- Shapes/Assets/TestsEditMode/CubeHandlerTests.cs
+++ Shapes/Assets/TestsEditMode/CubeHandlerTests.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 namespace Tests
 {
     public class CubeHandlerTests
     {
+        private CubeHandler CreateDefaultCubeHandler()
+        {
+            return new GameObject().AddComponent<CubeHandler>();
+        }
+
         [Test]
         public void HasSpeed_SpeedIsNotPositive_ReturnsFalse()
         {
-            var sut = new GameObject().AddComponent<CubeHandler>();
+            var sut = CreateDefaultCubeHandler();
             sut.RunTestingConstructor(0.0f);
 
             var result = sut.HasSpeed();
@@ -22,7 +24,7 @@ namespace Tests
         [Test]
         public void HasSpeed_SpeedIsPositive_ReturnsTrue()
         {
-            var sut = new GameObject().AddComponent<CubeHandler>();
+            var sut = CreateDefaultCubeHandler();
             sut.RunTestingConstructor(float.Epsilon);
 
             var result = sut.HasSpeed();
