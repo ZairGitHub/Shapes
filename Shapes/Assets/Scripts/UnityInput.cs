45020812ea0e7cd3b2e888e9ecb729ebc21d6c1c
Zair <michaelxzr@gmail.com>
2021-01-23 17:19:51 +0000

    Add properties to UnityInput.cs

--- Shapes/Assets/Scripts/UnityInput.cs
+++ Shapes/Assets/Scripts/UnityInput.cs
@@ -1,4 +1,8 @@
 ﻿public class UnityInput
 {
+    public string Horizontal { get; } = nameof(Horizontal);
 
+    public string Vertical { get; } = nameof(Vertical);
+
+    public string Jump { get; } = nameof(Jump);
 }
