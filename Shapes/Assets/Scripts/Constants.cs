9798697fe4cf8bf2d04ba6d1910599c88ec14a74
Zair <michaelxzr@gmail.com>
2021-01-23 18:57:57 +0000

    Add properties to Constants.cs

--- Shapes/Assets/Scripts/Constants.cs
+++ Shapes/Assets/Scripts/Constants.cs
@@ -3,14 +3,18 @@
 public class Constants : MonoBehaviour, IConstants
 {
     private GameObject _boundary;
-    private GameObject _boundaryNorth;
-    private GameObject _boundaryEast;
-    private GameObject _boundarySouth;
-    private GameObject _boundaryWest;
+    private Transform _boundaryNorth;
+    private Transform _boundaryEast;
+    private Transform _boundarySouth;
+    private Transform _boundaryWest;
 
     public float BoundaryWidth { get; private set; }
 
     public float BoundaryHeight { get; private set; }
+
+    public float FullBoundaryWidth { get; private set; }
+
+    public float FullBoundaryHeight { get; private set; }
 
     public float GameWidth { get; private set; }
 
@@ -21,13 +25,19 @@ public class Constants : MonoBehaviour, IConstants
         _boundary = GameObject.FindWithTag(Tags.Boundary);
         if (_boundary != null)
         {
-            _boundaryNorth = _boundary.transform.GetChild(0).gameObject;
-            _boundaryEast = _boundary.transform.GetChild(1).gameObject;
-            _boundarySouth = _boundary.transform.GetChild(2).gameObject;
-            _boundaryWest = _boundary.transform.GetChild(3).gameObject;
+            _boundaryNorth = _boundary.transform.GetChild(0);
+            _boundaryEast = _boundary.transform.GetChild(1);
+            _boundarySouth = _boundary.transform.GetChild(2);
+            _boundaryWest = _boundary.transform.GetChild(3);
+
+            FullBoundaryWidth = _boundaryEast.position.x - _boundaryWest.position.x;
+            FullBoundaryHeight = _boundaryNorth.position.y - _boundarySouth.position.y;
 
             BoundaryWidth = _boundaryEast.transform.position.x;
             BoundaryHeight = _boundaryNorth.transform.position.y;
+
+            Debug.Log($"FullWidth: {FullBoundaryWidth}, FullHeight: {FullBoundaryHeight}, " +
+                $"BoundaryWidth {BoundaryWidth}, BoundaryHeight {BoundaryHeight}");
         }
 
         GameWidth = BoundaryWidth * 2;
