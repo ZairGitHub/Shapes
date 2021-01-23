ba9f93c55d965f05b5a12e336bf7be2035487f7e
Zair <michaelxzr@gmail.com>
2021-01-23 16:27:23 +0000

    Add properties to Tags.cs

--- Shapes/Assets/Scripts/Tags.cs
+++ Shapes/Assets/Scripts/Tags.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
-
-public class Tags
+﻿public static class Tags
 {
+    public static string Constants { get; } = nameof(Constants);
 
+    public static string GameController { get; } = nameof(GameController);
 }
