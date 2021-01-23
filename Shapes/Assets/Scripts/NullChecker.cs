65d43668ba56d82f0625298b6f8cf05279d521ff
Zair <michaelxzr@gmail.com>
2021-01-23 12:57:31 +0000

    Add single argument TryFind() method to NullChecker.cs

--- Shapes/Assets/Scripts/NullChecker.cs
+++ Shapes/Assets/Scripts/NullChecker.cs
@@ -18,6 +18,18 @@ public static class NullChecker
         return component;
     }
 
+    public static Component TryFind<T>(string tag) where T : Component
+    {
+        try
+        {
+            return GameObject.FindWithTag(tag).GetComponent<T>();
+        }
+        catch (Exception)
+        {
+            return new GameObject().AddComponent<T>();
+        }
+    }
+
     public static Component TryFind<T>(string tag, GameObject gameObject) where T : Component
     {
         if (gameObject == null)
