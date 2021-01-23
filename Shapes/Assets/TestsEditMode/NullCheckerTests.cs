cc347b90126efd168a6e82343d49569053dffc06
Zair <michaelxzr@gmail.com>
2021-01-23 11:43:50 +0000

    Update NullCheckerTests.cs for single argument version of TryGet() method

--- Shapes/Assets/TestsEditMode/NullCheckerTests.cs
+++ Shapes/Assets/TestsEditMode/NullCheckerTests.cs
@@ -7,28 +7,19 @@ namespace Tests
     public class NullCheckerTests
     {
         private readonly GameObject _gameObject = new GameObject();
-        private readonly Component _component = new GameObject().AddComponent<Rigidbody>();
 
         [Test]
         public void TryGet_NullGameObject_ReturnsNull()
         {
-            var result = NullChecker.TryGet<Rigidbody>(null, _component);
+            var result = NullChecker.TryGet<Rigidbody>(null);
 
             Assert.That(result, Is.Null);
         }
 
         [Test]
-        public void NullComponent_ReturnsComponent()
+        public void NullComponent_GameObject_ReturnsComponent()
         {
-            var result = NullChecker.TryGet<Rigidbody>(_gameObject, null);
-
-            Assert.That(result, Is.TypeOf<Rigidbody>());
-        }
-
-        [Test]
-        public void TryGet_GameObjectAndComponent_ReturnsComponent()
-        {
-            var result = NullChecker.TryGet<Rigidbody>(_gameObject, _component);
+            var result = NullChecker.TryGet<Rigidbody>(_gameObject);
 
             Assert.That(result, Is.TypeOf<Rigidbody>());
         }
