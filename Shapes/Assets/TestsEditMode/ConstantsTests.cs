bfd53ffac155dd6c76c6d35862799aa5e17cfc95
Zair <michaelxzr@gmail.com>
2021-01-28 22:48:39 +0000

    Add [Test]s to ConstantsTests.cs

--- Shapes/Assets/TestsEditMode/ConstantsTests.cs
+++ Shapes/Assets/TestsEditMode/ConstantsTests.cs
@@ -32,7 +32,7 @@ namespace Tests
         }
 
         [Test]
-        public void Awake_BoundaryDoesNotExist_SetsBoundaryWidthToZero()
+        public void Awake_BoundaryGameDoesNotExist_SetsGameWidthToZero()
         {
             RemoveBoundaryGameTag();
             var sut = CreateDefaultConstants();
@@ -45,7 +45,7 @@ namespace Tests
         }
 
         [Test]
-        public void Awake_BoundaryDoesNotExist_SetsBoundaryHeightToZero()
+        public void Awake_BoundaryGameDoesNotExist_SetsGameHeightToZero()
         {
             RemoveBoundaryGameTag();
             var sut = CreateDefaultConstants();
@@ -58,7 +58,7 @@ namespace Tests
         }
 
         [Test]
-        public void Awake_BoundaryExists_SetsBoundaryWidthToBoundaryChildren()
+        public void Awake_BoundaryGameExists_SetsGameWidthToBoundaryChildren()
         {
             var boundary = GameObject.FindWithTag(Tags.BoundaryGame);
             var boundaryChild1 = boundary.transform.GetChild(1).position.x;
@@ -72,7 +72,7 @@ namespace Tests
         }
 
         [Test]
-        public void Awake_BoundaryExists_SetsBoundaryHeightToBoundaryChildren()
+        public void Awake_BoundaryGameExists_SetsGameHeightToBoundaryChildren()
         {
             var boundary = GameObject.FindWithTag(Tags.BoundaryGame);
             var boundaryChild1 = boundary.transform.GetChild(0).position.y;
@@ -86,7 +86,7 @@ namespace Tests
         }
 
         [Test]
-        public void Awake_SetsHalfBoundaryWidthToBoundaryWidthDividedByTwo()
+        public void Awake_SetsHalfGameWidthToGameWidthDividedByTwo()
         {
             var sut = CreateDefaultConstants();
             sut.runInEditMode = true;
@@ -97,7 +97,7 @@ namespace Tests
         }
 
         [Test]
-        public void Awake_SetsHalfBoundaryHeightToBoundaryHeightDividedByTwo()
+        public void Awake_SetsHalfGameHeightToGameHeightDividedByTwo()
         {
             var sut = CreateDefaultConstants();
             sut.runInEditMode = true;
@@ -105,6 +105,32 @@ namespace Tests
             var result = sut.HalfGameHeight;
 
             Assert.That(result, Is.EqualTo(sut.GameHeight / 2.0f));
+        }
+
+        [Test]
+        public void Awake_BoundaryViewDoesNotExist_SetsViewWidthToZero()
+        {
+            RemoveBoundaryViewTag();
+            var sut = CreateDefaultConstants();
+            sut.runInEditMode = true;
+
+            var result = sut.ViewWidth;
+
+            Assert.That(result, Is.Zero);
+            RestoreBoundaryViewTag();
+        }
+
+        [Test]
+        public void Awake_BoundaryViewDoesNotExist_SetsViewHeightToZero()
+        {
+            RemoveBoundaryViewTag();
+            var sut = CreateDefaultConstants();
+            sut.runInEditMode = true;
+
+            var result = sut.ViewHeight;
+
+            Assert.That(result, Is.Zero);
+            RestoreBoundaryViewTag();
         }
     }
 }
