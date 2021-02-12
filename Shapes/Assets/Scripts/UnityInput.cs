6ad9d5fc1a8a9e4a56265ca4edf73f2db33759fa
Zair <michaelxzr@gmail.com>
2021-02-12 23:18:46 +0000

    Convert getters into expression bodies in UnityInput.cs

--- Shapes/Assets/Scripts/UnityInput.cs
+++ Shapes/Assets/Scripts/UnityInput.cs
@@ -1,8 +1,8 @@
 ﻿public static class UnityInput
 {
-    public static string Horizontal { get; } = nameof(Horizontal);
+    public static string Horizontal => nameof(Horizontal);
 
-    public static string Vertical { get; } = nameof(Vertical);
+    public static string Vertical => nameof(Vertical);
 
-    public static string Jump { get; } = nameof(Jump);
+    public static string Jump => nameof(Jump);
 }
